b64a3095f064553aef675ec53c443c1571b88ff1
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-14 21:00:00 -0500

    add search and saved routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
 function App() {
     return (
-        <div>
-            hi
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/saved/" component={Saved} />
+                <Route path="*" component={Search} />
+            </Switch>
+        </Router>
     );
 }
 
