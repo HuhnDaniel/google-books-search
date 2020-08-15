3e5ef0e62595f4a01783f3d6cd0ed940983b5716
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-14 20:51:29 -0500

    set up tailwindcss

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./assets/tailwind/main.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
 registerServiceWorker();
