cbd759dbfe9009d34595294cb0a1ed7291fa2149
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-15 21:28:16 -0500

    set up with external api

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchArea from "../components/SearchArea";
 import SearchResults from "../components/SearchResults";
+import API from "../utils/API";
 
-function Search() {
-    return (
-        <main>
-            <SearchArea />
-            <SearchResults />
-        </main>
-    );
+class Search extends Component {
+    state = {
+        bookSearch: ""
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            bookSearch: e.target.value
+        });
+    };
+
+    handleSearchClick = async (e) => {
+        e.preventDefault();
+        console.log(this.state);
+
+        const books = await API.searchBooks(this.state.bookSearch);
+        console.log(books);
+    };
+
+    render() {
+        return (
+            <main>
+                <SearchArea handleSearchClick={this.handleSearchClick} handleChange={this.handleChange} />
+                <SearchResults />
+            </main>
+        );
+    }
 }
 
 export default Search;
