2b557f4e3e2c1baa9683b4fd0571805ef1bc1449
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-16 18:05:22 -0500

    update package.json

--- server.js
+++ server.js
@@ -9,17 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Define API routes here
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch");
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
